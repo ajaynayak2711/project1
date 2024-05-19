f2b76ba9bca65629aa37cd6779fa93c9c964875e
unknown <nayakajay142@gmail.com>
2024-05-19 18:16:27 +0530

    add table and install yajra

---
+++ database/migrations/2024_05_19_114816_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');			
+            $table->string('name', 64);
+            $table->string('email', 128);
+            $table->string('contact', 16);
+            $table->text('address');
+            $table->tinyInteger('status')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
